6c29403a046f612d66c6d9e40576ba360263a57e
Josh Wong <joshuarwong@outlook.com>
2022-07-09 17:14:23 +0900

    Update commit message; remove test path
    Update commit message and remove local test path

--- auto-csm.bat
+++ auto-csm.bat
@@ -1,10 +1,10 @@
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set datenow=%%c-%%a-%%b)
 :: Add the local file path after "cd " below to navigate to the folder where the files are
-cd "C:\Users\joshu\OneDrive\Documents\GitHub\kindle-book-highlights"
+cd 
 :: Gather all changes
 git add --all
 :: Commit all changes
-git commit -m %%datenow%%
+git commit -m "Update highlights and notes"
 :: Submit the pull request
 git push
