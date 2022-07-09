2fcdc4bca0ac0581c78f5387b6e22fe09bff6d15
Josh Wong <joshuarwong@outlook.com>
2022-07-09 17:50:17 +0900

    Revise comments; clarify parts to modify
    Revise comments and clarify parts to modify when using this script.

--- auto-csm.bat
+++ auto-csm.bat
@@ -1,7 +1,8 @@
 @echo off
+:: Get current date in MMDDYYYY format
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set datenow=%%c-%%a-%%b)
-:: Add the local file path after "cd " below to navigate to the folder where the files are
-cd 
+:: Navigate to the local folder where the files to be pushed are
+cd <ADD LOCAL PATH>
 :: Gather all changes
 git add --all
 :: Commit all changes
