e29d8019c30438b1599d33f35459b0b6a3b7a899
Josh Wong <joshuarwong@outlook.com>
2022-07-31 14:49:41 +0900

    Revise comments and instructions

--- auto-csm.bat
+++ auto-csm.bat
@@ -1,11 +1,11 @@
 @echo off
 :: Get current date in MMDDYYYY format
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set datenow=%%c-%%a-%%b)
-:: Navigate to the local folder where the files to be pushed are
-cd <ADD LOCAL PATH>
+:: Navigate to the local folder where the files to be pushed are; be sure to use back slashes between folder names
+cd "<INSERT FULL LOCAL PATH>"
 :: Gather all changes
 git add --all
-:: Commit all changes
-git commit -m "Update highlights and notes"
+:: Commit all changes, with a commit message that is about 50 characters long
+git commit -m "<ADD COMMIT MESSAGE>"
 :: Submit the pull request
 git push
